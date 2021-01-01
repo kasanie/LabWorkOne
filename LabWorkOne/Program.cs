207ff215cb1254d8ec89ce03b3348dea92b16511
ssplant <76212491+ssplant@users.noreply.github.com>
2021-01-01 22:17:53 +0900

    Add project files.

---
+++ LabWorkOne/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace LabWorkOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Таблица умножения:");
+            int firstMulitiplier, secondMultiplier;
+            for (firstMulitiplier = 2; firstMulitiplier <= 9; firstMulitiplier++)
+            {
+                for (secondMultiplier = 2; secondMultiplier <= 9; secondMultiplier++)
+                    Console.WriteLine(firstMulitiplier + " * " + secondMultiplier + " = "+(firstMulitiplier*secondMultiplier));
+            }
+        }
+    }
+}
